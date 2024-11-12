e6458a6c97e553166a4a4e45a0fafbf754d5b5f4
PC <kordianpawelec2@gmail.com>
2024-11-12 13:39:51 +0000

    War Init

---
+++ kordianspetitions/src/main/java/net/javaguides/kordianspetitions/KordianspetitionsApplication.java
@@ -0,0 +1,13 @@
+package net.javaguides.kordianspetitions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KordianspetitionsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KordianspetitionsApplication.class, args);
+	}
+
+}
